c4f4ffe67777e91d55b54ff6a3b1ec00a9f27db5
Rishabh <rishabhgarai33@gmail.com>
2024-07-16 13:23:48 +0530

    i am done navbar

--- index.js
+++ index.js
@@ -39,3 +39,9 @@ window.onload = function() {
     typeWriter();
 }
 
+function toggleMenu() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuContent = document.querySelector('.menu-content');
+    menuIcon.classList.toggle('active');
+    menuContent.classList.toggle('active');
+}
